dfd03bc4409573b39d465860a2f954d2af140e65
laura reded <lauramelo2308@hotmail.com>
2024-10-15 17:36:48 -0300

    inicializando

---
+++ API/Models/Tarefa.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace API.Models;
+
+public class Tarefa
+{
+    public int TarefaId { get; set;}
+
+    public string? Nome { get; set;}
+
+    public DateTime CriadoEm { get; set;} = DateTime.Now;
+
+}
