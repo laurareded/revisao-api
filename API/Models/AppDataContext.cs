475225955e70cc41c0889dae2f99504db533a543
laura reded <lauramelo2308@hotmail.com>
2024-10-15 18:00:07 -0300

    migracoes e primeira funcionalidade

---
+++ API/Models/AppDataContext.cs
@@ -0,0 +1,15 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+public class AppDataContext : DbContext
+{
+    public DbSet<Tarefa> Tarefas { get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=app.db");
+    }
+
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,19 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Revisao da prova");
+
+app.MapPost("/api/tarefas/cadastrar", ([FromBody] Tarefa tarefa, [FromServices] AppDataContext ctx) =>
+{
+    ctx.Tarefas.Add(tarefa);
+    ctx.SaveChanges();
+    return Results.Created("", tarefa);
+}
+
+);
 
 app.Run();
